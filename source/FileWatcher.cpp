6125b4a39745e9e21fce4712b6dbb0456a5505ae
Johann Muszynski <johann.muszynski@gmail.com>
2015-10-24 15:14:27 +0300

    reset project

--- source/FileWatcher.cpp
+++ source/FileWatcher.cpp
@@ -1,23 +1,23 @@
 /**
-	Copyright (c) 2009 James Wynn (james@jameswynn.com)
+    Copyright (c) 2009 James Wynn (james@jameswynn.com)
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
 */
 
 #include <FileWatcher/FileWatcher.h>
@@ -37,47 +37,47 @@
 namespace FW
 {
 
-	//--------
-	FileWatcher::FileWatcher()
-	{
-		mImpl = new FILEWATCHER_IMPL();
-	}
+    //--------
+    FileWatcher::FileWatcher()
+    {
+        mImpl = new FILEWATCHER_IMPL();
+    }
 
-	//--------
-	FileWatcher::~FileWatcher()
-	{
-		delete mImpl;
-		mImpl = 0;
-	}
+    //--------
+    FileWatcher::~FileWatcher()
+    {
+        delete mImpl;
+        mImpl = 0;
+    }
 
-	//--------
-	WatchID FileWatcher::addWatch(const String& directory, FileWatchListener* watcher)
-	{
-		return mImpl->addWatch(directory, watcher, false);
-	}
+    //--------
+    WatchID FileWatcher::addWatch(const String& directory, FileWatchListener* watcher)
+    {
+        return mImpl->addWatch(directory, watcher, false);
+    }
 
-	//--------
-	WatchID FileWatcher::addWatch(const String& directory, FileWatchListener* watcher, bool recursive)
-	{
-		return mImpl->addWatch(directory, watcher, recursive);
-	}
+    //--------
+    WatchID FileWatcher::addWatch(const String& directory, FileWatchListener* watcher, bool recursive)
+    {
+        return mImpl->addWatch(directory, watcher, recursive);
+    }
 
-	//--------
-	void FileWatcher::removeWatch(const String& directory)
-	{
-		mImpl->removeWatch(directory);
-	}
+    //--------
+    void FileWatcher::removeWatch(const String& directory)
+    {
+        mImpl->removeWatch(directory);
+    }
 
-	//--------
-	void FileWatcher::removeWatch(WatchID watchid)
-	{
-		mImpl->removeWatch(watchid);
-	}
+    //--------
+    void FileWatcher::removeWatch(WatchID watchid)
+    {
+        mImpl->removeWatch(watchid);
+    }
 
-	//--------
-	void FileWatcher::update()
-	{
-		mImpl->update();
-	}
+    //--------
+    void FileWatcher::update()
+    {
+        mImpl->update();
+    }
 
 };//namespace FW
